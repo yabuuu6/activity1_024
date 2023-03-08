0389b1cf72c9f60e9d33079e2f4fb8d064e5d097
buyaramadhan <ytchochote@gmail.com>
2023-03-08 16:23:48 +0700

    first commit

---
+++ Latihan1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+    //numeric Luas, P, L
+    double Luas, P, L ;
+    //Display, "Masukan Panjang : "
+    cout << " Masukan Panjangnya : ";
+    //Accept P
+    cin >> P; 
+    //Display, "Masukan Lebarnya : "
+    cout << "Masukkan Lebarnya : ";
+    //Accept L
+    cin >> L;
+    //Computer Luas = P*L
+    Luas = P*L;
+    //Display, " Luasnya : " + Luas
+    cout << "Luasnya : " << Luas;
+}//end
+
